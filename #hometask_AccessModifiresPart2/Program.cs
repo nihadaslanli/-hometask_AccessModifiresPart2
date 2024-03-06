83fb9bb983d37b673ba73fdd340ab66002680b1b
nihadaslanli <nihadsasl@code.edu.az>
2024-03-06 18:14:08 +0400

    Add project files.

---
+++ #hometask_AccessModifiresPart2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _hometask_AccessModifiresPart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City city=new City();
+            Console.WriteLine("seher daxil edin");
+            city.Name = Console.ReadLine();
+            Console.WriteLine("population daxil edin");
+            city.Population=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"seher:{city.Name}+population:{city.Population}");
+        }
+    }
+}
